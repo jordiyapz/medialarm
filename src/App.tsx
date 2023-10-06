aa87d3a222b91b631670ff7666353223ab023682
Jordi Yaputra <jordiyaputra@gmail.com>
2023-10-06 11:58:21 +0700

    install material ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,8 @@
 import { Alarm, AlarmConfig, Timelet } from "./components/alarm";
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 
 function App() {
   const alarmConfig: AlarmConfig = {};
