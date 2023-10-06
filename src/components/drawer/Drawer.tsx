e320e8d0cdbd96081f3dc863369c9ffa4da65b2f
Jordi Yaputra <jordiyaputra@gmail.com>
2023-10-06 16:46:49 +0700

    add alarm list in drawer

--- src/components/drawer/Drawer.tsx
+++ src/components/drawer/Drawer.tsx
@@ -7,8 +7,10 @@ import {
   Box,
   ListItem,
   ListItemButton,
-  ListItemText,
   Divider,
+  Switch,
+  Stack,
+  FormLabel,
 } from "@mui/material";
 import { DrawerHeader } from ".";
 import ChevronRightIcon from "@mui/icons-material/ChevronRight";
@@ -39,14 +41,27 @@ export const AlarmDrawer = ({
         },
       }}
     >
-      <Box>
-        <Typography>Alarm List</Typography>
+      <Box mx={2} mt={3}>
+        <Typography variant="h1" fontWeight={700} fontSize="1.2rem">
+          Alarm List
+        </Typography>
       </Box>
       <List>
-        <ListItem disablePadding> 
-        <ListItemButton>
-          <ListItemText>hehehe</ListItemText>
-        </ListItemButton>
+        <ListItem disablePadding sx={{ alignItems: "stretch" }}>
+          <ListItemButton>
+            <Stack>
+              <Typography color={pallete.grey400}>Sun, 13 Oct 2023</Typography>
+              <Typography fontSize="1.4rem" fontWeight={100}>
+                08:00
+              </Typography>
+              <Typography color={pallete.grey400} fontWeight={100}>
+                ring once
+              </Typography>
+            </Stack>
+          </ListItemButton>
+          <FormLabel sx={{ pr: 3, display: "flex", alignItems: "center" }}>
+            <Switch edge="end" />
+          </FormLabel>
         </ListItem>
       </List>
       <DrawerHeader>
---
+++ src/components/footer/NextAlarm.tsx
@@ -0,0 +1,50 @@
+import {
+  Box, IconButton,
+  Stack,
+  Typography,
+  useTheme
+} from "@mui/material";
+import NotificationsOffIcon from "@mui/icons-material/NotificationsOff";
+
+export function NextAlarm() {
+  const theme = useTheme();
+  return (
+    <Stack direction="row" alignItems="center">
+      <Box>
+        <Typography
+          component="h1"
+          fontSize="1.2rem"
+          color={theme.palette.text.secondary}
+        >
+          Next Alarm
+        </Typography>
+        <Stack direction="row" alignItems="flex-end" gap={1.5}>
+          <Box>
+            <Typography fontSize="1.4rem" lineHeight={1.1}>
+              16:20
+            </Typography>
+            <Typography fontSize=".8rem" fontWeight={100} color="primary">
+              (+1 day)
+            </Typography>
+          </Box>
+          <Box>
+            <Typography
+              variant="subtitle2"
+              fontWeight={100}
+              fontSize=".9rem"
+              color={theme.palette.text.secondary}
+            >
+              will ring in
+            </Typography>
+            <Typography variant="body2" fontWeight={500}>
+              1 day, 1h 14m 45s
+            </Typography>
+          </Box>
+        </Stack>
+      </Box>
+      <IconButton aria-label="turn-off" size="large" color="secondary">
+        <NotificationsOffIcon />
+      </IconButton>
+    </Stack>
+  );
+}
